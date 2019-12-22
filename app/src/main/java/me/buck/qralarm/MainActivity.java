3abb493b0367dd4bed00b260bb312e020ba4c199
Buck <coderbuck@163.com>
2019-12-22 17:38:18 +0800

    add qr

---
+++ app/src/main/java/me/buck/qralarm/MainActivity.java
@@ -0,0 +1,97 @@
+package me.buck.qralarm;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.king.zxing.CaptureActivity;
+import com.king.zxing.Intents;
+
+import java.util.List;
+
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
+
+public class MainActivity extends AppCompatActivity implements EasyPermissions.PermissionCallbacks {
+
+    public static final int REQUEST_CODE_SCAN = 0X01;
+
+    public static final int RC_CAMERA = 0X01;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkCameraPermissions();
+            }
+        });
+    }
+
+    /**
+     * 检测拍摄权限
+     */
+    @AfterPermissionGranted(RC_CAMERA)
+    private void checkCameraPermissions(){
+        String[] perms = {Manifest.permission.CAMERA};
+        if (EasyPermissions.hasPermissions(this, perms)) {//有权限
+            startScan(null,null);
+        } else {
+            // Do not have permissions, request them now
+            EasyPermissions.requestPermissions(this, "xxxxxxxx",
+                    RC_CAMERA, perms);
+        }
+    }
+
+    /**
+     * 扫码
+     * @param cls
+     * @param title
+     */
+    private void startScan(Class<?> cls,String title){
+        Intent intent = new Intent(this, CaptureActivity.class);
+        startActivityForResult(intent,REQUEST_CODE_SCAN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK && data!=null){
+            switch (requestCode){
+                case REQUEST_CODE_SCAN:
+                    String result = data.getStringExtra(Intents.Scan.RESULT);
+                    Toast.makeText(this,result, Toast.LENGTH_SHORT).show();
+                    break;
+
+            }
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        // Forward results to EasyPermissions
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
+
+    @Override
+    public void onPermissionsGranted(int requestCode, @NonNull List<String> perms) {
+
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode, @NonNull List<String> perms) {
+
+    }
+}
